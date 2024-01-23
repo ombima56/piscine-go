c3e43ae5cd7e48352a4ca178174629af23d34b18
hiombima <ombimahillary52@gmail.com>
2024-01-23 10:47:09 +0300

    file to look a files in current folder in directory and its sub-folder

--- lookagain.sh
+++ lookagain.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -name "*.sh" | sed 's/\.sh$//' | sed 's/^\.///' | awk -f'/' '{print $NF}' sort -r | sed 's/\$$//'|
+find . -type f -name "*.sh" | sed 's/\.sh$//' | sed 's/^\.///' | awk -f'/' '{print $NF}' sort -r | sed 's/\$$//'
