092c249d7d286f30261e3891f13fff9248492120
hiombima <ombimahillary52@gmail.com>
2024-01-23 10:11:50 +0300

    someone familiar

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json |jq --arg name "$HERO_ID" '.[] | select( .id == ($name|tonumber))'
+curl https://learn.zone01kisumu.ke/assets/superhero/all.json |jq "[map(select(.id == ${HERO_ID}))] | .[] | .[] .connections.relatives" | tr -d '"'
