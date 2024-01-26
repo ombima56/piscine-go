4fdf84b2d858e140edcc378384bb1ee41865e2fd
hiombima <ombimahillary52@gmail.com>
2024-01-26 18:35:53 +0300

    simulating the behaviour of the atoi

--- basicatoi.go
+++ basicatoi.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 func BasicAtoi(s string) int {
 	var answer int
@@ -8,10 +6,4 @@ func BasicAtoi(s string) int {
 		answer = answer*10 + int(char-'0')
 	}
 	return answer
-}
-
-func main() {
-	fmt.Println(BasicAtoi("12345"))
-	fmt.Println(BasicAtoi("0000000012345"))
-	fmt.Println(BasicAtoi("000000"))
 }
