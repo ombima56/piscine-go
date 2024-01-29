0273f5a17941a4a81cd49037d1a539ed33dfe575
hiombima <ombimahillary52@gmail.com>
2024-01-29 15:34:43 +0300

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,15 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
 	var number int = 1
 	for q := 1; q <= nb; q++ {
+		if number < 0 {
+			return 0
+		}
 		number *= q
 	}
 	return number
