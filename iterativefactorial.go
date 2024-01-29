9a306415f5945b178ac27d467ee1bcfb4fca2bdf
hiombima <ombimahillary52@gmail.com>
2024-01-29 12:57:27 +0300

    iteratvefactorial

---
+++ iterativefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb < 0 || nb > 12 {
+		return 0
+	}
+
+	var number int = 1
+	for q := 1; q <= nb; q++ {
+		number *= q
+	}
+	return number
+}
