d3c8a2b4ccb3d0a4224b0218334fb25fc45a9d54
hiombima <ombimahillary52@gmail.com>
2024-01-29 16:10:43 +0300

    recursivefactorial

---
+++ recursivefactorial.go
@@ -0,0 +1,17 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb == 1 {
+		return 1
+	}
+	if nb < 0 {
+		return 0
+	}
+
+	if nb > 1 {
+		nb *= RecursiveFactorial(nb - 1)
+		return nb
+	} else {
+		return 0
+	}
+}
