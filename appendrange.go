fd99da423608b984e72cf02ee4e4aa56239e9989
hiombima <ombimahillary52@gmail.com>
2024-02-03 13:37:45 +0300

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -6,8 +6,8 @@ func AppendRange(min, max int) []int {
 	}
 
 	var answer []int
-	for i := min; i <= max; i++ {
-		answer = append(answer, i-1)
+	for i := min; i < max; i++ {
+		answer = append(answer, i)
 	}
 	return answer
 }
