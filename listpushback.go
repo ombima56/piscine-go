2754280391cfb55f0910b381679a82b3afe4690c
hiombima <ombimahillary52@gmail.com>
2024-02-12 17:59:17 +0300

    listpushback

---
+++ listpushback.go
@@ -0,0 +1,22 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	node := &NodeL{Data: data, Next: nil}
+	if l.Head == nil {
+		l.Head = node
+		l.Tail = node
+	} else {
+		l.Tail.Next = node
+		l.Tail = node
+	}
+}
