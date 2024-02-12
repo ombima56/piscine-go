20c574581e256b84b805510f8de6081713e26e3c
hiombima <ombimahillary52@gmail.com>
2024-02-12 20:10:13 +0300

    listsize

--- listsize.go
+++ listsize.go
@@ -21,3 +21,13 @@ func ListSize(l *List) int {
 	}
 	return size
 }
+
+func ListPushFront(l *List, data interface{}) {
+	q := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = q
+		return
+	}
+	q.Next = l.Head
+	l.Head = q
+}
