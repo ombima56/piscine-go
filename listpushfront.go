9441713bbd3953498632a3b092411cc35fc808bf
hiombima <ombimahillary52@gmail.com>
2024-02-12 19:35:41 +0300

    listpushfront

---
+++ listpushfront.go
@@ -0,0 +1,19 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type LinkedList struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *LinkedList, data interface{}) {
+	newNode := &NodeL{Data: data, Next: l.Head}
+	l.Head = newNode
+	if l.Tail == nil {
+		l.Tail = newNode
+	}
+}
