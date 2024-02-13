0095ee36732d79faed40c1934aa2e86f79cc46a3
hiombima <ombimahillary52@gmail.com>
2024-02-13 16:56:47 +0300

    listlast

--- listlast.go
+++ listlast.go
@@ -1,8 +1,19 @@
 package piscine
 
-func listLast(l *List) interface{} {
+func ListLast(l *List) interface{} {
 	if l.Head == nil {
 		return nil
 	}
 	return l.Tail.Data
+}
+
+func ListPushBack(l *List, data interface{}) {
+	node := &NodeL{Data: data, Next: nil}
+	if l.Head == nil {
+		l.Head = node
+		l.Tail = node
+	} else {
+		l.Tail.Next = node
+		l.Tail = node
+	}
 }
