dc8f4562c84022ca424dee002454cab653bc6485
hiombima <ombimahillary52@gmail.com>
2024-02-15 02:14:59 +0300

    btreetransplant

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,16 +1,18 @@
 package piscine
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	if root == node {
-		return rplc
+	if node == nil {
+		return root
 	}
-	
-	parent := BTreeSearchParent(root, node)
-	if parent.Left == node {
-		parent.Left = rplc
+	if node == root {
+		root = rplc
+	} else if node == node.Parent.Left {
+		node.Parent.Left = rplc
+	} else {
+		node.Parent.Right = rplc
 	}
-	if parent.Right == node {
-		parent.Right = rplc
+	if rplc != nil {
+		rplc.Parent = node.Parent
 	}
 	return root
 }
